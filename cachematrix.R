f064b2dba33ea1243b3483188a880f6cff4ac571
Courtney Murphy <courtneym@slalom.com>
2015-06-17 21:00:51 -0500

    Completed Functions for Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i<-NULL
+  set<-function(m){
+    x<<-m
+    i<<-NULL
+  }
+  get<-function() x
+  invmatrix<-function(inv) i<<-inv
+  getinv<-function() i
+  list(set=set, get=get, invmatrix=invmatrix, 
+       getinv= getinv)
 }
 
 
@@ -12,4 +21,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i<- x$getinv()
+  if(!is.null(i)){
+    message("getting cached data") 
+    return(i)
+  }
+  
+  data<- x$get()
+  i<-solve(data, ...)
+  x$invmatrix(i)
+  i
 }
